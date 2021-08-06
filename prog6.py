5e3de9d0b078380c61e77d0b46463b8ed0f333f3
Brandon <okatro350@gmail.com>
2021-08-06 13:28:30 -0500

    initial commit

---
+++ prog6.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ## ###############################################################
+# prog6.py
+#
+# Author: Covarrubias Minez Brandon Iván
+# License: MIT
+# GitHub: https://github.com/okatro/Programa-6
+#
+# Convert numbers from the decimal system to the roman system
+#
+# ## ###############################################################
+import os
+import re
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
